4d492af60b0b2e63cbec7fdc4077eb40ef6211db
yusei00nagai <yusei.nagai1013@gmail.com>
2023-06-12 12:12:35 +0900

    適当

--- src/modules/profile/About.tsx
+++ src/modules/profile/About.tsx
@@ -1,9 +1,18 @@
-import { Box, SxProps, Theme } from "@mui/material";
+import { Box, SxProps, Theme, Typography } from "@mui/material";
+import profileImg from "../../resources/work1/images/about.jpg";
 
 export const About = () => {
   return (
     <Box className="About" sx={sx}>
-      アバウト
+      <Box className="About-Title">About</Box>
+      <Box className="About-Content">
+        <img className="About-Content-Image" src={ profileImg } alt="profile" />
+        <Box className="About-Content-Profile">
+          <Typography className="About-Content-Name">KAKERU MIYAICHI</Typography>
+          <Box className="About-Content-Text">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</Box>
+        </Box>
+        
+      </Box>
     </Box>
   );
 };
@@ -11,5 +20,30 @@ export const About = () => {
 const sx: SxProps<Theme> = {
   "&.About" : {
     width: "100%",
+  },
+  ".About-Title" : {
+    textAlign: "center",
+    margin: "40px",
+    fontSize: "40px",
+    borderBottom: "1px solid #000",
+  },
+  ".About-Content" : {
+    display: "flex",
+    justifyContent: "center",
+  },
+  ".About-Content-Image" : {
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%",
+    border: "1px solid #000",
+    marginRight: "30px",
+  },
+  ".About-Content-Profile" : {
+    width: "500px",
+  },
+  ".About-Content-Name" : {
+    marginBottom: "10px",
+  },
+  ".About-Content-Text" : {
   },
 };
--- src/modules/profile/Mainvisual.tsx
+++ src/modules/profile/Mainvisual.tsx
@@ -1,16 +1,20 @@
-import { Box, SxProps, Theme } from "@mui/material";
-import bicycle from "../../resources/work1/images/bicycle1.jpg"
+import { Box, SxProps, Theme, containerClasses } from "@mui/material";
+import bicycle from "../../resources/work1/images/bicycle1.jpg";
 
 export const Mainvisual = () => {
   return (
     <Box className="Mainvisual" sx={sx}>
-      <img src="{ bicycle }" alt="bicycle" />
+      <img className="Mainvisual-Image" src={ bicycle } alt="bicycle" />
     </Box>
   );
 };
 
 const sx: SxProps<Theme> = {
   "&.Mainvisual" : {
+    width: "100%",
+  },
+  ".MainVisual-Image" : {
+    objectFit: "contain",
     width: "100%",
   },
 };
